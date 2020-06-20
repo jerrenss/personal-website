6dfdfb282fc586e04644cb54e3b5c72ec8542314
jerrenss <>
2020-06-20 23:43:17 +0800

    Added Background

---
+++ src/views/Banner/Text.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import { Box, Container, Typography, Paper } from '@material-ui/core'
+import 'fontsource-roboto'
+
+const useStyles = makeStyles((theme) => ({
+  defaultText: {
+    fontFamily: 'fontsource-roboto',
+    color: 'white',
+  },
+}))
+
+const Text = (props) => {
+  const classes = useStyles()
+
+  return (
+    <Box>
+      <Typography variant="h6" className={classes.defaultText}>
+        Greetings, I'm
+      </Typography>
+      <Typography variant="h2" className={classes.defaultText}>
+        Gerren Seow
+      </Typography>
+      <Typography variant="h6" className={classes.defaultText}>
+        A Web Development Enthusiast
+      </Typography>
+    </Box>
+  )
+}
+
+export default Text
---
+++ src/views/Home.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import makeStyles from '@material-ui/core/styles/makeStyles'
+import { Box, Card, Container, Typography, Paper } from '@material-ui/core'
+import Text from './Banner/Text'
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  banner: {
+    height: '100vh',
+    backgroundImage: `url('/home/banner.png')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    '& .MuiBox-root': {
+      marginBottom: theme.spacing(40),
+    },
+  },
+  image: {
+    width: '100%',
+  },
+}))
+
+const Home = (props) => {
+  const classes = useStyles()
+
+  return (
+    <Box>
+      {/* Home Banner Section */}
+      <Paper className={classes.banner}>
+        <Text />
+        {/* <img
+          src="/home/banner.png"
+          alt="Home Banner"
+          className={classes.image}
+        /> */}
+      </Paper>
+      <Container></Container>
+    </Box>
+  )
+}
+
+export default Home
